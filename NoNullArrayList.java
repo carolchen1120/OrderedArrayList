5914623ea9cdea64d5c500e7930583a9137a600f
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-09 13:08:24 -0500

    Add files via upload
    I added in the default and the set and add methods. I have not yet added anything to the set and add methods.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,15 @@
-public class NoNullArrayList<T> extends ArrayList<T> {
+import java.util.ArrayList;
 
+public class NoNullArrayList<T> extends ArrayList<T> {
+  public NoNullArrayList() {
+    super.ArrayList();
+  }
+
+  public set() {
+    // if (T == null)
+  }
+
+  public add() {
+
+  }
 }
