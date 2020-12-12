807e5ed6c0c0e8209766f1638ebc46711beacc96
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-12 16:42:02 -0500

    Add files via upload
    This is the new class OrderedArrayList. I created it along with both its default and startingCapacity constructors.

---
+++ OrderedArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  OrderedArrayList() {
+    super();
+  }
+
+  OrderedArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  
+}
