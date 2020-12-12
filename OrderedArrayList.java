2cc02684689740149121379968f8cc3e5fb1a608
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-12 17:26:23 -0500

    Add files via upload
    I added return statements for the three functions add(), add(index), and set(index, value).

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,28 +9,31 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
-  public set(int index, T value) {
+  public T set(int index, T value) {
     if (OrderedArrayList() == null) {
       throw new IllegalArgumentException();
     } else {
       super.remove(index);
       super.add(index, value);
+      return value;
     }
   }
 
-  public add() {
+  public boolean add() {
     if (OrderedArrayList() == null) {
       throw new IllegalArgumentException();
     } else {
       super.add();
+      return true;
     }
   }
 
-  public add(int index) {
+  public boolean add(int index) {
     if (OrderedArrayList() == null) {
       throw new IllegalArgumentException();
     } else {
       super.add(index);
+      return true;
     }
   }
 }
